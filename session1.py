0979b46153042131746335ac8d9775acb56fe8a5
asb <abaidas.ieu2024@student.ie.edu>
2026-01-20 16:59:34 +0100

    my first commit ever

---
+++ session1.py
@@ -0,0 +1,24 @@
+#this is my first code
+#print('Hello world')
+print(7+6)
+print('and John asked: "how are you?"')
+print('hello'' ''world')
+
+print('3 + 4 = ', 3+4, 'and 5 X 6 = ', 5*6)
+
+# difference between sentence with and without seperator
+print('I have $', 100+100, '.')
+print('I have $', 100+100, '.', sep='')
+print('I have $', 100+100, '.', sep='!!')
+# end is used to tell print what to do after it prints
+#example of end, end can be whatever I want
+print('1', end='')
+print('2', end='')
+print('3', end=', x6m comp')
+
+print('4',end='\n\n\n')
+print('5', end='')
+
+print(2+3*6/2)
+print(4/2)
+print(4//2)
